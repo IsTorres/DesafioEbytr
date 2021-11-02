20e30f86450893f17a5197c79c20dff2cd87200b
isTorres <ismaeltorres612@gmail.com>
2021-11-02 01:01:37 -0300

    comunicação de client e servidor

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ const {
   getStatusCode, 
 } = require('http-status-codes');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.status(StatusCodes.OK).json(ReasonPhrases.OK);
+  res.json({ message: 'server on' })
 })
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
