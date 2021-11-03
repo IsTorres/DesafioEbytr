792ce2cef8c0ecd4089cd59473522f646626e35f
isTorres <ismaeltorres612@gmail.com>
2021-11-02 23:40:07 -0300

    cria rota /tarefas p/ listar tarefas no banco de dados

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const {
   getStatusCode, 
 } = require('http-status-codes');
 
+const {
+  criaTarefa,
+  todasTarefas,
+} = require('./controller/tarefaController');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -13,5 +18,7 @@ const app = express();
 app.get('/api', (req, res) => {
   res.json({ message: 'server on' })
 })
+
+app.get('/tarefas', todasTarefas)
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
