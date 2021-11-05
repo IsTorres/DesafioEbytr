a414cd1f424d3c5a5b7da6cefb59f48cb8e5c95a
isTorres <ismaeltorres612@gmail.com>
2021-11-05 01:24:48 -0300

    conserta o model de tarefas

--- server/controller/tarefaController.js
+++ server/controller/tarefaController.js
@@ -7,16 +7,16 @@ const tarefaService = require('../service/tarefaService');
 
 const todasTarefas = async (req, res) => {
   const tarefas = await tarefaService.todasTarefas();
-
+  
   if (!tarefas) return res.status(StatusCodes.OK).json({ message: 'vazio' });
-  return res.status(StatusCodes.OK).json(ReasonPhrases.OK);
+  return res.status(StatusCodes.OK).json(tarefas);
 };
 
 const criaTarefa = async (req, res) => {
-  const { tarefa } = req.query('tarefa');
-  const { status } = req.query('status');
+  const { tarefa, status } = req.body;
 
   const criar = await tarefaService.criaTarefa(tarefa, status);
+  
   if (!criar) return res.status(StatusCodes.BAD_REQUEST).json(ReasonPhrases.BAD_REQUEST);
   return res.status(StatusCodes.CREATED).json(ReasonPhrases.CREATED);
 };
