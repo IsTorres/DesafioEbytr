f2cb66a5bff3f1eff41cf3ff94ef7b30b2295a5b
isTorres <ismaeltorres612@gmail.com>
2021-11-05 10:45:05 -0300

    remove useeffect inutilizado

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Form from './components/Form';
-import Table from './components/Table';
+import Lista from './components/Table';
 import './CSS/App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-  }, []);
-
-  return (
+ return (
     <div className="App">
-      <p>{ !data ? 'Loading...' : data }</p>
       <Form />
-      <Table />
+      <Lista />
     </div>
   );
 }
